e08902db16546d9639a5d676025d7a0e1e299612
J A <jarata@gmail.com>
2019-02-26 13:27:46 -0800

    added morgan added knex and setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(morgan('dev'));
 
 // endpoints here
 
